54a51169b3d2f508718969af837e6c0e88ee30e9
Roxy Moe <crt@adastra7.net>
2023-03-03 13:36:00 +0100

    i push shit out my ass

---
+++ Project1/src/maumaumau/net/basic/Main.java
@@ -0,0 +1,22 @@
+package maumaumau.net.basic;
+import java.util.Scanner;
+
+/** Mein zweites Java-Programm
+    @author     Maumaumau.net
+    @version    1.0
+    @see        java.util.Scanner
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String myname;
+        String mysurname;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please tell daddy your First Name:");
+        myname = scanner.nextLine();
+        System.out.print("Please tell daddy your Last Name:");
+        mysurname = scanner.nextLine();
+        System.out.print("Hi " + myname + " " + mysurname + ", you're looking real submissive and breedable today heheh ;)");
+    }
+}
